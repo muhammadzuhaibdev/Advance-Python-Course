b7bec000bf50b197bac36036363002a8379c17ec
Hafiz-Zuhaib-Dev <hafizzuhaibazaad457@gmail.com>
2026-03-05 14:08:58 +0500

    Class-07 Added

---
+++ class-07/class07.py
@@ -0,0 +1,11 @@
+studentNames = ["Huzaifa", "Zuhaib", "Ali", "Hamza", "Abdullah"]
+listOfNumbers = [10,20,30,40,50]
+# result = studentNames.append("Hadi")
+# studentNames.append("Hassan")
+# studentNames.append("Ali")
+# result = studentNames.pop(0)
+# result =studentNames.insert(2, "Ahmed")
+# result = studentNames.extend(listOfNumbers)
+studentNames.remove("Hamza")
+
+print(studentNames)
